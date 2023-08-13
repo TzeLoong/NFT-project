b25dd4b9725e0781c3e081557ccab456e375c9cd
TzeLoong <tzeloong12@gmail.com>
2023-08-14 00:27:45 +0800

    Implemented backend database, nft apis and deployed smart contract

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,6 +1,7 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const PRIVATE_KEY = "YOUR PRIVATE KEY";
+const PRIVATE_KEY =
+  "a22497351a3ecdd4016604065a7fa33c1024f9890b7897465d96b94ee76cfcc3";
 const RPC_URL = "https://rpc.ankr.com/polygon_mumbai";
 module.exports = {
   defaultNetwork: "polygon_mumbai",
